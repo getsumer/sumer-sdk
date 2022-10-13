47a59176cac5df74f9161513d6e5cc6556b5bea6
Mario Maita <thetechpomelo@gmail.com>
2022-10-13 17:34:41 +0200

    build

--- build/main/Api.d.ts
+++ build/main/Api.d.ts
@@ -1,5 +1,4 @@
 export declare class Api {
-    static _instance: Api;
     private headers;
     private url;
     constructor(key: string);
--- src/Api.ts
+++ src/Api.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export class Api {
-    static _instance: Api;
+    //static _instance: Api;
     private headers: any;
     private url:string = 'http://127.0.0.1:3000'
     constructor(key: string) {
-        if (Api._instance) {
-            return Api._instance
-        }
-        Api._instance = this;
+        // if (Api._instance) {
+        //     return Api._instance
+        // }
+      //  Api._instance = this;
         this.headers = {
             Authorization: `Bearer ${key}`
         }
