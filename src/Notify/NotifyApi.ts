83fb8f49f0a85eda24876b84ff952b2faf3361d3
mmo <thetechpomelo@gmail.com>
2022-10-18 15:20:41 +0200

    .

--- src/Notify/NotifyApi.ts
+++ src/Notify/NotifyApi.ts
@@ -3,6 +3,7 @@ import bowser from 'bowser'
 import { ContractError } from '../Errors/ContractError'
 import { ProviderError } from '../Errors/ProviderError'
 import { Notify } from './Notify'
+import { v4 } from 'uuid';
 
 export class NotifyApi implements Notify {
     private client: Api
@@ -11,16 +12,23 @@ export class NotifyApi implements Notify {
     }
 
     txHash(message: any): void {
+        const  id = v4().toString()
+
         console.log("tx hash data ", message)
+
         const data = {
+            id,
             ...message,
             metadata: this.meta()
         }
         this.client.sendTxHash(data.txHash, data)
     }
     providerError(message: any): void {
+        const  id = v4().toString()
+
         console.log("provider error: ", message)
         const data = {
+            id,
             userAddress: message.address,
             code: message.code,
             message: message.message,
@@ -30,11 +38,14 @@ export class NotifyApi implements Notify {
         this.client.sendProviderError(data)
     }
     public error(msg: ContractError | ProviderError) {
+        const  id = v4().toString()
+
         const log = {
+            id,
             userAddress: msg.address,
             message: msg.toString(),
-            timestamp: Date.now(),
-            wallet: msg.address,
+            //timestamp: Date.now(),
+            //wallet: msg.address,
             metadata: this.meta()
         }
         this.client.send(log)
