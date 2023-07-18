4f1e0bf1419877dfbfd98fe7ba7421ad6d1c729a
cito-lito <thetechpomelo@gmail.com>
2023-07-18 13:15:49 +0200

    refactor: ensure args data is sent

--- src/observers/TransactionObserver.ts
+++ src/observers/TransactionObserver.ts
@@ -6,15 +6,19 @@ export class TransactionObserver extends Observer {
     if (this.isTransaction(execution)) {
       const { result } = execution
       const methodArgs = this.getMethodArgs(execution)
+      const args = methodArgs[0]
+        ? (JSON.parse(methodArgs[0]) as Record<string, string>)
+        : (methodArgs as Record<string, string>)
+
       await this.telemetryService.trackTransaction({
         chainId: this.parseNumber(result['chainId']) || this.getChainId(execution),
         hash: this.getTransactionHash(result),
 
-        fromAddress: methodArgs['from'] ?? this.getWalletAddress(execution),
-        toAddress: methodArgs['to'] ?? undefined,
-        value: this.parseBigNumber(methodArgs['value']) ?? undefined,
-        data: methodArgs['data'] ?? undefined,
-        gas: methodArgs['gas'] ?? undefined,
+        fromAddress: args['from'] ?? this.getWalletAddress(execution),
+        toAddress: args['to'] ?? undefined,
+        value: this.parseBigNumber(args['value']) ?? undefined,
+        data: args['data'] ?? undefined,
+        gas: args['gas'] ?? undefined,
         rpcMethodName: this.getMethodName(execution),
         rpcMethodArgs: Array.isArray(methodArgs) ? methodArgs : [JSON.stringify(methodArgs)],
         error: this.getTransactionError(result),
